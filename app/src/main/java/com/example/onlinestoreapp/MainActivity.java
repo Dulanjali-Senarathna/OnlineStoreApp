55173e23b1546b7f5ae07406d2d0a1623fe27960
Dulanjali-Senarathna <59603716+KandabadaSenarathna@users.noreply.github.com>
2020-09-13 17:54:38 +0530

    Bottom Navigation View Done

--- app/src/main/java/com/example/onlinestoreapp/MainActivity.java
+++ app/src/main/java/com/example/onlinestoreapp/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.onlinestoreapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_bar);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.all:
+                        bottomNavigationView.setItemBackgroundResource(R.color.allbottom);
+                        return true;
+
+                    case  R.id.catone:
+                        bottomNavigationView.setItemBackgroundResource(R.color.catone);
+                        return true;
+
+                    case R.id.cattwo:
+                        bottomNavigationView.setItemBackgroundResource(R.color.cattwo);
+                        return true;
+
+                        default:
+                            return false;
+                }
+
+            }
+        });
     }
 }
